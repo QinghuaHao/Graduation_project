fcd89acfb8f2463b75a88f025d2104098c0776b4
Qinghua Hao <qinghua_hao@gmail.com>
2024-07-09 15:45:06 +0100

    update: Add the function of collecting data, modify the video recognition,

--- data_collection/data_collection.py
+++ data_collection/data_collection.py
@@ -2,11 +2,14 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pandas as pd
+import os
 import warnings
-warnings.filterwarnings('ignore', category=UserWarning, module='google.protobuf.symbol_database') 
+warnings.filterwarnings('ignore', category=UserWarning,
+                        module='google.protobuf.symbol_database')
+
 
 class DataCollection:
-    def __init__(self, width=640, height=480):
+    def __init__(self, width=640, height=480, sequence_length=60):
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(max_num_hands=2,
                                          min_detection_confidence=0.5,
@@ -14,81 +17,53 @@ class DataCollection:
         self.mp_drawing = mp.solutions.drawing_utils
         self.width = width
         self.height = height
+        self.recording = False
         self.previous_hand_position = None
+        self.initial_hand_wrist_coords = None
+        self.sequence_length = sequence_length
+        self.recorded_data = []
+        self.landmark_trail = []  # 用于记录landmark 8的轨迹
+        self.standard_gestures_dir = '../data/standard_gestures/'
+        if not os.path.exists(self.standard_gestures_dir):
+            os.makedirs(self.standard_gestures_dir)
 
-    def process_video(self, cap):
-        landmark_trail = []
-        frame_count = 0  # 添加帧计数器
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("Cannot read frame from video stream")
-                break
-            frame = cv2.flip(frame, 1)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = self.hands.process(frame)
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            frame_landmarks_data = []
-            if results.multi_hand_landmarks:
-                if self.previous_hand_position is None:
-                    self.previous_hand_position = results.multi_hand_landmarks[
-                        0].landmark[self.mp_hands.HandLandmark.WRIST]
-                    initial_hand_wrist_coords = (int(
-                        self.previous_hand_position.x * frame.shape[1]), int(self.previous_hand_position.y * frame.shape[0]))
-                else:
-                    min_distance = float('inf')
-                    closest_hand_landmarks = None
-                    for hand_landmarks in results.multi_hand_landmarks:
-                        wrist = hand_landmarks.landmark[self.mp_hands.HandLandmark.WRIST]
-                        wrist_coords = (
-                            int(wrist.x * frame.shape[1]), int(wrist.y * frame.shape[0]))
-                        distance = np.linalg.norm(
-                            np.array(initial_hand_wrist_coords) - np.array(wrist_coords))
-                        if distance < min_distance:
-                            min_distance = distance
-                            closest_hand_landmarks = hand_landmarks
-                    if closest_hand_landmarks:
-                        self.previous_hand_position = closest_hand_landmarks.landmark[
-                            self.mp_hands.HandLandmark.WRIST]
-                        self.mp_drawing.draw_landmarks(
-                            frame, closest_hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
-                        for landmark_id, landmark in enumerate(closest_hand_landmarks.landmark):
-                            landmark_data = {
-                                'frame': frame_count,  # 添加帧编号
-                                'hand': 0,
-                                'landmark': landmark_id,
-                                'x': landmark.x,
-                                'y': landmark.y,
-                            }
-                            frame_landmarks_data.append(landmark_data)
-                            if landmark_id == 8:
-                                x, y = int(
-                                    landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
-                                if len(landmark_trail) > 0:
-                                    cv2.line(
-                                        frame, landmark_trail[-1], (x, y), (0, 255, 0), 2)
-                                landmark_trail.append((x, y))
-                                for pt in landmark_trail:
-                                    cv2.circle(frame, pt, 3, (0, 255, 0), -1)
-            for i in range(1, len(landmark_trail)):
-                cv2.line(frame, landmark_trail[i-1],
-                         landmark_trail[i], (0, 255, 0), 2)
-            cv2.imshow('Camera Feed', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+    def start_recording(self):
+        self.recording = True
+        self.recorded_data = []
+        self.landmark_trail = []
 
-            df = pd.DataFrame(frame_landmarks_data)
-            gesture = self.detect_gesture(df)
-            yield df, gesture
+    def stop_recording(self, gesture_name):
+        self.recording = False
+        if self.recorded_data:
+            df = pd.DataFrame(self.recorded_data)
+            normalized_df = self.normalize_sequence_length(df)
+            self.save_standard_gesture(gesture_name, normalized_df)
+        self.landmark_trail = []
 
-            frame_count += 1  # 增加帧计数器
+    def normalize_sequence_length(self, df):
+        current_length = len(df)
+        if current_length > self.sequence_length:
+            return df.iloc[:self.sequence_length]  # 截断
+        else:
+            pad_length = self.sequence_length - current_length
+            padding = pd.DataFrame([df.iloc[-1]] * pad_length)  # 用最后一帧填充
+            return pd.concat([df, padding], ignore_index=True)
 
-        cap.release()
-        cv2.destroyAllWindows()
+    def save_standard_gesture(self, gesture_name, df):
+        file_path = os.path.join(
+            self.standard_gestures_dir, f"{gesture_name}.csv")
+        df.to_csv(file_path, index=False)
+        print(f"Gesture {gesture_name} saved to {file_path}")
 
-    def store_file(self, name, df):
-        file = f"../data/data_csv/{name}.csv"
-        df.to_csv(file, index=False)
+    def load_standard_gestures(self):
+        gestures = {}
+        for file_name in os.listdir(self.standard_gestures_dir):
+            if file_name.endswith('.csv'):
+                gesture_name = file_name[:-4]
+                df = pd.read_csv(os.path.join(
+                    self.standard_gestures_dir, file_name))
+                gestures[gesture_name] = df[['x','y']]
+        return gestures
 
     def detect_gesture(self, df):
         if df.empty:
@@ -162,6 +137,99 @@ class DataCollection:
             return True
         return False
 
+    # def draw_trajectory(self, frame):
+    #     frame_height, frame_width, _ = frame.shape
+    #     for i in range(1, len(self.recorded_data)):
+    #         if self.recorded_data[i - 1] is None or self.recorded_data[i] is None:
+    #             continue
+    #         pt1 = (int(self.recorded_data[i - 1]['x'] * frame_width),
+    #                int(self.recorded_data[i - 1]['y'] * frame_height))
+    #         pt2 = (int(self.recorded_data[i]['x'] * frame_width),
+    #                int(self.recorded_data[i]['y'] * frame_height))
+    #         cv2.line(frame, pt1, pt2, (0, 255, 0), 2)
+    #         cv2.circle(frame, pt2, 3, (0, 0, 255), -1)
+    #     return frame
+
+    def process_frame(self, frame, frame_count):
+        frame = cv2.flip(frame, 1)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        frame_landmarks_data = []
+
+        if results.multi_hand_landmarks:
+            if self.previous_hand_position is None:
+                self.previous_hand_position = results.multi_hand_landmarks[
+                    0].landmark[self.mp_hands.HandLandmark.WRIST]
+                self.initial_hand_wrist_coords = (int(self.previous_hand_position.x * frame.shape[1]),
+                                                  int(self.previous_hand_position.y * frame.shape[0]))
+            else:
+                min_distance = float('inf')
+                closest_hand_landmarks = None
+                for hand_landmarks in results.multi_hand_landmarks:
+                    wrist = hand_landmarks.landmark[self.mp_hands.HandLandmark.WRIST]
+                    wrist_coords = (
+                        int(wrist.x * frame.shape[1]), int(wrist.y * frame.shape[0]))
+                    distance = np.linalg.norm(
+                        np.array(self.initial_hand_wrist_coords) - np.array(wrist_coords))
+                    if distance < min_distance:
+                        min_distance = distance
+                        closest_hand_landmarks = hand_landmarks
+                if closest_hand_landmarks:
+                    self.previous_hand_position = closest_hand_landmarks.landmark[
+                        self.mp_hands.HandLandmark.WRIST]
+                    self.mp_drawing.draw_landmarks(
+                        frame, closest_hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+                    for landmark_id, landmark in enumerate(closest_hand_landmarks.landmark):
+                        landmark_data = {
+                            'frame': frame_count,
+                            'hand': 0,
+                            'landmark': landmark_id,
+                            'x': landmark.x,
+                            'y': landmark.y,
+                        }
+                        frame_landmarks_data.append(landmark_data)
+                        if self.recording and landmark_id==8:
+                            self.recorded_data.append(landmark_data)
+                            x, y = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
+                            self.landmark_trail.append((x, y))
+
+        # 绘制轨迹
+        if self.recording and len(self.landmark_trail) > 1:
+            for i in range(1, len(self.landmark_trail)):
+                cv2.line(frame, self.landmark_trail[i - 1], self.landmark_trail[i], (0, 255, 0), 2)
+
+
+        return frame, frame_landmarks_data
+
+    def process_video(self, cap):
+        frame_count = 0
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Cannot read frame from video stream")
+                break
+
+            frame, frame_landmarks_data = self.process_frame(
+                frame, frame_count)
+            cv2.imshow('Camera Feed', frame)
+
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q'):
+                break
+            elif key & 0xFF == ord('r'):
+                self.start_recording()
+                print("Started recording...")
+            elif key & 0xFF == ord('s'):
+                gesture_name = input("Enter gesture name to save: ")
+                self.stop_recording(gesture_name)
+
+            frame_count += 1
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
 
@@ -169,8 +237,5 @@ if __name__ == '__main__':
         print("Cannot open camera")
     else:
         data_collector = DataCollection()
-        for frame_df, gesture in data_collector.process_video(cap):
-            print("Frame landmarks:\n", frame_df)
-            print("Detected gestures:", gesture)
-            # data_collector.store_file("hand_landmarks", frame_df)
+        data_collector.process_video(cap)
     cap.release()
