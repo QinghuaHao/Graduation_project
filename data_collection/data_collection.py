ab83740a835443e0766696a84f0eaa66d214d90c
Qinghua Hao <hqh19990719@163.com>
2024-07-09 03:38:33 +0100

    t can be done by detecting gestures, in which the video will not be closed, it will be opened all the time, and the hand feature point data will be collected until the end of a certain action. Now the thumb is up and down for opening and ending, and the collected data will be processed

--- data_collection/data_collection.py
+++ data_collection/data_collection.py
@@ -1,62 +1,41 @@
 import cv2
-from h11 import Data
 import mediapipe as mp
-import time
 import numpy as np
 import pandas as pd
-# determine hand_id
+import warnings
+warnings.filterwarnings('ignore', category=UserWarning, module='google.protobuf.symbol_database') 
 
-
-class DataCollection():
-
-    def __init__(self, camera_num=0, width=640, height=480, fps=30, duration=10, num_points=40):
+class DataCollection:
+    def __init__(self, width=640, height=480):
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(max_num_hands=2,
                                          min_detection_confidence=0.5,
                                          min_tracking_confidence=0.5)
         self.mp_drawing = mp.solutions.drawing_utils
-        self.landmarks_data = []
         self.width = width
         self.height = height
-        self.fps = fps
-        self.duration = duration
         self.previous_hand_position = None
-        self.num_points = num_points
-        self.camera_num = camera_num
 
-    def open_camera(self):
-        cap = cv2.VideoCapture(self.camera_num)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.height)
-        cap.set(cv2.CAP_PROP_FPS, self.fps)
-
-        if not cap.isOpened():
-            print("can not open camera")
-            return
-
-        start_time = time.time()
-        frame_count = 0
+    def process_video(self, cap):
         landmark_trail = []
-        points_collected = 0
-
+        frame_count = 0  # 添加帧计数器
         while True:
             ret, frame = cap.read()
             if not ret:
-                print("camera can not open")
+                print("Cannot read frame from video stream")
                 break
             frame = cv2.flip(frame, 1)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = self.hands.process(frame)
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            frame_landmarks_data = []
             if results.multi_hand_landmarks:
                 if self.previous_hand_position is None:
-                    # Record the initial hand position (wrist coordinates)
                     self.previous_hand_position = results.multi_hand_landmarks[
                         0].landmark[self.mp_hands.HandLandmark.WRIST]
                     initial_hand_wrist_coords = (int(
                         self.previous_hand_position.x * frame.shape[1]), int(self.previous_hand_position.y * frame.shape[0]))
                 else:
-                    # Find the hand closest to the previous hand position
                     min_distance = float('inf')
                     closest_hand_landmarks = None
                     for hand_landmarks in results.multi_hand_landmarks:
@@ -75,13 +54,13 @@ class DataCollection():
                             frame, closest_hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
                         for landmark_id, landmark in enumerate(closest_hand_landmarks.landmark):
                             landmark_data = {
-                                'frame': frame_count,
-                                'hand': 0,  # Always 0 since we're only tracking one hand
+                                'frame': frame_count,  # 添加帧编号
+                                'hand': 0,
                                 'landmark': landmark_id,
                                 'x': landmark.x,
                                 'y': landmark.y,
                             }
-                            self.landmarks_data.append(landmark_data)
+                            frame_landmarks_data.append(landmark_data)
                             if landmark_id == 8:
                                 x, y = int(
                                     landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
@@ -91,34 +70,107 @@ class DataCollection():
                                 landmark_trail.append((x, y))
                                 for pt in landmark_trail:
                                     cv2.circle(frame, pt, 3, (0, 255, 0), -1)
-                                points_collected += 1
-                                df = pd.DataFrame(self.landmarks_data)
-                                if points_collected >= self.num_points:
-                                    break
-                        if points_collected >= self.num_points:
-                            break
-                    if points_collected >= self.num_points:
-                        break
             for i in range(1, len(landmark_trail)):
                 cv2.line(frame, landmark_trail[i-1],
                          landmark_trail[i], (0, 255, 0), 2)
             cv2.imshow('Camera Feed', frame)
-            # print(self.landmarks_data)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            frame_count += 1
-            if time.time() - start_time > self.duration:
-                break
+
+            df = pd.DataFrame(frame_landmarks_data)
+            gesture = self.detect_gesture(df)
+            yield df, gesture
+
+            frame_count += 1  # 增加帧计数器
+
         cap.release()
-        cv2.destroyWindow('Camera Feed')
-        return df
+        cv2.destroyAllWindows()
 
-    def store_file(self, name):
+    def store_file(self, name, df):
         file = f"../data/data_csv/{name}.csv"
-        self.open_camera().to_csv(file)
+        df.to_csv(file, index=False)
 
+    def detect_gesture(self, df):
+        if df.empty:
+            return 'None'
+
+        landmarks = {row['landmark']: (row['x'], row['y'])
+                     for _, row in df.iterrows()}
+        if self.is_ok_gesture(landmarks):
+            return 'OK'
+        elif self.is_victory_gesture(landmarks):
+            return 'Victory'
+        elif self.is_thumbs_up_gesture(landmarks):
+            return 'Thumbs Up'
+        elif self.is_thumbs_down_gesture(landmarks):
+            return 'Thumbs Down'
+        elif self.is_fist_gesture(landmarks):
+            return 'Fist'
+        elif self.is_five_gesture(landmarks):
+            return 'Five'
+        else:
+            return 'None'
+
+    def is_ok_gesture(self, landmarks):
+        if 4 in landmarks and 8 in landmarks:
+            thumb_tip = np.array(landmarks[4])
+            index_tip = np.array(landmarks[8])
+            distance = np.linalg.norm(thumb_tip - index_tip)
+            if distance < 0.05:  # Threshold for OK gesture
+                return True
+        return False
+
+    def is_victory_gesture(self, landmarks):
+        if 8 in landmarks and 12 in landmarks:
+            index_tip = np.array(landmarks[8])
+            middle_tip = np.array(landmarks[12])
+            distance = np.linalg.norm(index_tip - middle_tip)
+            if distance > 0.1:  # Threshold for Victory gesture
+                return True
+        return False
+
+    def is_thumbs_up_gesture(self, landmarks):
+        if 4 in landmarks and 3 in landmarks and 2 in landmarks:
+            thumb_tip = np.array(landmarks[4])
+            thumb_ip = np.array(landmarks[3])
+            thumb_mcp = np.array(landmarks[2])
+            # Check if thumb is pointing upwards
+            if thumb_tip[1] < thumb_ip[1] < thumb_mcp[1]:
+                return True
+        return False
+
+    def is_thumbs_down_gesture(self, landmarks):
+        if 4 in landmarks and 3 in landmarks and 2 in landmarks:
+            thumb_tip = np.array(landmarks[4])
+            thumb_ip = np.array(landmarks[3])
+            thumb_mcp = np.array(landmarks[2])
+            # Check if thumb is pointing downwards
+            if thumb_tip[1] > thumb_ip[1] > thumb_mcp[1]:
+                return True
+        return False
+
+    def is_fist_gesture(self, landmarks):
+        for i in range(5, 21):
+            if i in landmarks:
+                x, y = landmarks[i]
+                if y < landmarks[0][1]:
+                    return False
+        return True
+
+    def is_five_gesture(self, landmarks):
+        if all(i in landmarks for i in range(5, 21)):
+            return True
+        return False
 
 if __name__ == '__main__':
-    dc = DataCollection(width=640, height=480, fps=60,
-                        duration=10, num_points=80)
-    dc.store_file(name=1)
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Cannot open camera")
+    else:
+        data_collector = DataCollection()
+        for frame_df, gesture in data_collector.process_video(cap):
+            print("Frame landmarks:\n", frame_df)
+            print("Detected gestures:", gesture)
+            # data_collector.store_file("hand_landmarks", frame_df)
+    cap.release()
--- main.py
+++ main.py
@@ -5,7 +5,59 @@ file = os.getcwd()
 sys.path.append(file)
 
 from data_collection.data_collection import DataCollection
+import cv2
+import pandas as pd
+import numpy as np
 
-dc = DataCollection(width=640, height=480, fps=60,
-                        duration=10, num_points=80)
-print(dc.open_camera())
+
+'''
+1.数据流
+2.if 手势ok 收集数据流,直到victory结束,返回数据包
+3.根据数据包形成标准轨迹
+4.进行匹配
+5,匹配完成以后输出
+'''
+
+dc = DataCollection()
+cap =cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Camera can not open")
+else:
+    recording = False
+    collected_data = []
+    i=0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can not receive frame")
+            break
+        
+        frame = cv2.flip(frame, 1)  # 正确翻转镜头
+        
+        for df, gesture in dc.process_video(cap):
+            print(f"gesture_is: {gesture}")
+            # print("data_frame_is")
+            # print(df)
+            
+            if gesture == 'Thumbs Up':
+                recording = True
+                collected_data = []  # 开始记录时清空之前的记录数据
+
+            if recording:
+                collected_data.append(df)  # 记录当前帧的特征点数据
+            
+            if gesture == 'Thumbs Down' and recording:
+                recording = False
+                i+=1
+                # 将收集到的数据组合成一个 DataFrame
+                full_data = pd.concat(collected_data, ignore_index=True)
+                print(f"Collected data{i}:")
+                print(full_data)
+                collected_data = []
+            
+
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cap.destroyAllWindows('Camera Feed')
